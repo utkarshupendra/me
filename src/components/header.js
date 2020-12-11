554938ba0d027a553bd944b7d603adaf223e92dd
uupendra1 <utkarsh.upendra@isaacapp.in>
2020-12-11 16:04:00 +0530

    Using HashRouter

--- src/components/header.js
+++ src/components/header.js
@@ -2,30 +2,29 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Articles from './articles';
-import { Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
-import Switch from 'react-bootstrap/esm/Switch';
+import { HashRouter, Route, Link } from "react-router-dom";
+
 
 export default function Header() {
-    return(<div>
-            <Navbar className="header" variant="light">
-  <Navbar.Brand href="/">Utkarsh Upendra</Navbar.Brand>
+    return(
+        <HashRouter basename="/me">
+            <div>
+            <Navbar className="header" variant="light" fixed="top">
+  <Link to="/">
+  <Navbar.Brand>Utkarsh Upendra</Navbar.Brand></Link>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="/articles">Articles</Nav.Link>
+      <Nav.Link as={Link} to="/articles" >Articles</Nav.Link>
     </Nav>
   </Navbar.Collapse>
   </Navbar>
   <div>
-  <BrowserRouter>
-  <Switch>
           <Route path="/articles">
             <Articles />
           </Route>
-          </Switch>
-          </BrowserRouter>
           </div>
           </div>
+          </HashRouter>
     );
 }
